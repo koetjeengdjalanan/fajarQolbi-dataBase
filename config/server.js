f409c127349a83c98a629b8a1e36f70282733b38
koetjeengdjalanan <martinsambulare@gmail.com>
2021-09-13 05:12:21 +0000

    Initial Commit | add all necesary db for Web @(Mon Sep 13 12:11:32 2021 +0700)

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2f06e849858f175056d1c9bf16640581'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4dffab95-d02b-457c-ae0d-e34719ddf50b'
+};
